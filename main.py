9f4ed9b492e27da96ed09694ae479204b9685d93
Prola <pranshupratap@icloud.com>
2026-03-31 01:22:09 +0530

    Uploading Backend for Render
    Added main files

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+from fastapi import FastAPI
+import numpy as np
+from tensorflow.keras.models import load_model
+
+app = FastAPI()
+
+# 🔥 Load model once
+model = load_model("ecg_sliding_model.keras")
+
+@app.get("/")
+def home():
+    return {"status": "ECG API Running"}
+
+@app.post("/predict")
+def predict(data: dict):
+    try:
+        # ===== STEP 1: Extract =====
+        arr = np.array(data["data"])
+
+        # ===== STEP 2: Validate =====
+        if len(arr) != 200:
+            return {"error": "Input must be 200 values"}
+
+        # ===== STEP 3: Normalize safely =====
+        mean = np.mean(arr)
+        std = np.std(arr)
+
+        if std == 0:
+            return {"error": "Invalid ECG signal"}
+
+        arr = (arr - mean) / std
+
+        # ===== STEP 4: Reshape =====
+        arr = arr.reshape(1, 200, 1)
+
+        # ===== STEP 5: Predict =====
+        pred = model.predict(arr, verbose=0)[0][0]
+
+        # ===== STEP 6: Decision =====
+        threshold = 0.4
+        result = "Abnormal" if pred > threshold else "Normal"
+
+        return {
+            "result": result,
+            "confidence": float(pred)
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
